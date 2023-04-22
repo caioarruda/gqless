8efa5e847db06f8d7ac6d374daa986e5005903a0
Caio Arruda <caioarruda@gmail.com>
2023-04-22 11:25:57 -0300

    publish teste

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,4 +1,4 @@
-const { getConfig } = require('test-utils/jest.config.js');
+const { getConfig } = require('@gqless-transport-ws/test-utils/jest.config.js');
 
 module.exports = getConfig({
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],

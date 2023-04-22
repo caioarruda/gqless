5a58917129b70cbe4dbf47eb58b86457f75409ac
Caio Arruda <caioarruda@gmail.com>
2023-04-22 13:49:35 -0300

    update subs client

--- packages/subscriptions/src/subscription/protocol.ts
+++ packages/subscriptions/src/subscription/protocol.ts
@@ -4,11 +4,11 @@ export const GQL_CONNECTION_ERROR = 'connection_error'; // Server -> Client
 export const GQL_CONNECTION_KEEP_ALIVE = 'ka'; // Server -> Client
 
 export const GQL_CONNECTION_TERMINATE = 'connection_terminate'; // Client -> Server
-export const GQL_START = 'start'; // Client -> Server
-export const GQL_DATA = 'data'; // Server -> Client
+export const GQL_START = 'subscribe'; // Client -> Server
+export const GQL_DATA = 'next'; // Server -> Client
 export const GQL_ERROR = 'error'; // Server -> Client
 export const GQL_COMPLETE = 'complete'; // Server -> Client
-export const GQL_STOP = 'stop'; // Client -> Server
+export const GQL_STOP = 'complete'; // Client -> Server
 
 export const GRAPHQL_WS = 'graphql-ws';
 export const GRAPHQL_TS_WS = 'graphql-transport-ws';

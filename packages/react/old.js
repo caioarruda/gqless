eb81ed09f06974bfde070d83a2fce643958ce924
Caio Arruda <caioarruda@gmail.com>
2023-04-22 11:13:21 -0300

    react 1.0.2

--- packages/react/old.js
+++
@@ -1,6 +0,0 @@
-const { getConfig } = require('test-utils/jest.config.js');
-
-module.exports = getConfig({
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  testEnvironment: 'jsdom',
-});

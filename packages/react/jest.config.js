7f5aa6b179d3a6bbeda9d40e1f902f658643b97d
Caio Arruda <caioarruda@gmail.com>
2023-04-22 11:17:28 -0300

    publish teste

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,48 +1,4 @@
-/**
- * @type {import("@jest/types").Config.InitialOptions}
- */
-const defaultConfig = {
-  globals: {
-    'ts-jest': {},
-  },
-  transform: {
-    '.(ts|tsx)$': require.resolve('ts-jest/dist'),
-  },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.ts', '**/test/**/*.test.tsx'],
-  testURL: 'http://localhost',
-  testPathIgnorePatterns: ['/node_modules/', '/test/generated'],
-  collectCoverage: true,
-  collectCoverageFrom: ['./src/**/*.ts', './src/**/*.tsx', '!**/*.d.ts'],
-  testTimeout: 10000,
-  watchPlugins: [
-    require.resolve('jest-watch-typeahead/filename'),
-    require.resolve('jest-watch-typeahead/testname'),
-  ],
-  restoreMocks: true,
-};
-
-exports.default = defaultConfig;
-
-exports.getConfig = function getConfig(
-  /**
-   * @type {import("@jest/types").Config.InitialOptions}
-   */
-  config = {}
-) {
-  /**
-   * @type {import("@jest/types").Config.InitialOptions}
-   */
-  const newConfig = {
-    ...defaultConfig,
-    ...config,
-  };
-
-  return newConfig;
-};
-
+const { getConfig } = require('test-utils/jest.config.js');
 
 module.exports = getConfig({
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
